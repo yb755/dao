43a099e65a8fe2f0965a3549514a696876f69843
yebing <yebing@vvsmark.com>
2018-07-10 14:12:37 +0800

    update

---
+++ dao-mybatis/src/main/java/com/vbgps/dao/mybatis/MybatiesReadWritePlugin.java
@@ -0,0 +1,48 @@
+package com.vbgps.dao.mybatis;
+
+import java.util.Properties;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MybatiesReadWritePlugin implements Interceptor {
+
+	private Logger logger = LoggerFactory.getLogger(MybatiesReadWritePlugin.class);
+
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+		MetaObject metaStatementHandler = SystemMetaObject.forObject(statementHandler);
+		while (metaStatementHandler.hasGetter("h")) {
+			Object object = metaStatementHandler.getValue("h");
+			metaStatementHandler = SystemMetaObject.forObject(object);
+		}
+		// 分离最后一个代理对象的目标类
+		while (metaStatementHandler.hasGetter("target")) {
+			Object object = metaStatementHandler.getValue("target");
+			metaStatementHandler = SystemMetaObject.forObject(object);
+		}
+		MappedStatement mappedStatement = (MappedStatement) metaStatementHandler.getValue("delegate.mappedStatement");
+		String sqlId = mappedStatement.getId();
+		logger.debug("MybatiesReadWritePlugin##########sqlId:{}", sqlId);
+		return invocation.proceed();
+	}
+
+	@Override
+	public Object plugin(Object target) {
+		return Plugin.wrap(target, this);
+	}
+
+	@Override
+	public void setProperties(Properties properties) {
+
+	}
+
+}
