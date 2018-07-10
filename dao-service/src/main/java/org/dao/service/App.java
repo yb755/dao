2022317d7271be25cca8def6fb1e2b416701910f
yebing <yebing@vvsmark.com>
2018-07-10 09:15:45 +0800

    update

---
+++ dao-service/src/main/java/org/dao/service/App.java
@@ -0,0 +1,13 @@
+package org.dao.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
